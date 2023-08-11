82d78a5638649b131730992314daf1592f2a7cb4
Mileta <avramovic.mileta@gmail.com>
2023-08-11 10:53:42 +0200

    changing the place of the button and the counter

--- lib/image_search.dart
+++ lib/image_search.dart
@@ -9,7 +9,6 @@ import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'dart:html' as html;
 
-
 import 'package:permission_handler/permission_handler.dart';
 import 'custom_widget.dart';
 import 'liked_images.dart';
@@ -22,7 +21,7 @@ class ImageSearch extends StatefulWidget {
 
   // List<Message> get likedMessages => null;
 
- // const ImageSearch({super.key});
+  // const ImageSearch({super.key});
 
   @override
   // ignore: library_private_types_in_public_api
@@ -49,13 +48,13 @@ class _ImageSearchState extends State<ImageSearch> {
     super.initState();
     loadJsonData();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _showTermsAndConditionsDialog(context); // Show the dialog when the widget is initialized
+      _showTermsAndConditionsDialog(
+          context); // Show the dialog when the widget is initialized
     });
 
     // WidgetsBinding.instance!.addPostFrameCallback((_) { The '!' will have no effect because the receiver can't be null.
     //   _showTermsAndConditionsDialog(); // Show the dialog when the widget is initialized
     // });
-
   }
 
   @override
@@ -64,6 +63,7 @@ class _ImageSearchState extends State<ImageSearch> {
     focusNode.dispose();
     super.dispose();
   }
+
   void _showTermsAndConditionsDialog(BuildContext context) {
     showDialog(
       context: context,
@@ -92,11 +92,12 @@ class _ImageSearchState extends State<ImageSearch> {
                 SizedBox(height: 10),
                 Text(
                   'Tip: Click the reload button after agreeing to these terms and conditions to refresh the content. Use that button frequently if image takes too long to load or search some term then click x and everything should work fine. ',
-                  style: TextStyle(color: Colors.blue, fontStyle: FontStyle.italic),
+                  style: TextStyle(
+                      color: Colors.blue, fontStyle: FontStyle.italic),
                 ),
               ],
             ),
-          ), 
+          ),
           actions: [
             TextButton(
               onPressed: () {
@@ -114,7 +115,6 @@ class _ImageSearchState extends State<ImageSearch> {
     );
   }
 
-
   void searchMessages() {
     final query = searchController.text.toLowerCase();
     if (query.isNotEmpty) {
@@ -128,6 +128,7 @@ class _ImageSearchState extends State<ImageSearch> {
     focusNode.requestFocus();
     setState(() {});
   }
+
   void loadNextBatch() {
     int start = currentBatchIndex * batchSize;
     int end = min(start + batchSize, messages.length);
@@ -165,9 +166,9 @@ class _ImageSearchState extends State<ImageSearch> {
     }
   }
 
-
   void loadRandomImage() {
-    if (visibleMessages.length <= batchSize && currentBatchIndex * batchSize < messages.length) {
+    if (visibleMessages.length <= batchSize &&
+        currentBatchIndex * batchSize < messages.length) {
       loadNextBatch(); // Load the next batch if needed
     }
     if (visibleMessages.isNotEmpty) {
@@ -179,10 +180,9 @@ class _ImageSearchState extends State<ImageSearch> {
     setState(() {});
   }
 
-
   Future<void> loadJsonData() async {
     final fileNameString =
-    await rootBundle.loadString('lib/assets/file_names.txt');
+        await rootBundle.loadString('lib/assets/file_names.txt');
     final fileNames = LineSplitter.split(fileNameString);
     for (var fileName in fileNames) {
       final jsonString = await rootBundle.loadString('lib/assets/$fileName');
@@ -208,52 +208,53 @@ class _ImageSearchState extends State<ImageSearch> {
           messages); // Call the function when messages are loaded
     }
   }
-Future<void> downloadImage(String url) async {
-  if (kIsWeb) {
-    // Web platform code
-    var filename = url.substring(url.lastIndexOf("/") + 1);
-    html.AnchorElement(href: url)
-      ..setAttribute('download', filename)
-      ..click();
-  } else {
-    // Mobile platform code
-    var status = await Permission.storage.status;
-    if (!status.isGranted) {
-      await Permission.storage.request();
-      status = await Permission.storage.status;
-    }
 
-    if (status.isGranted) {
-      final response = await http.get(Uri.parse(url));
+  Future<void> downloadImage(String url) async {
+    if (kIsWeb) {
+      // Web platform code
+      var filename = url.substring(url.lastIndexOf("/") + 1);
+      html.AnchorElement(href: url)
+        ..setAttribute('download', filename)
+        ..click();
+    } else {
+      // Mobile platform code
+      var status = await Permission.storage.status;
+      if (!status.isGranted) {
+        await Permission.storage.request();
+        status = await Permission.storage.status;
+      }
 
-      if (response.statusCode == 200) {
-        final documentDirectory = await getApplicationDocumentsDirectory();
-        final file = File('${documentDirectory.path}/image_${DateTime.now().millisecondsSinceEpoch}.png');
+      if (status.isGranted) {
+        final response = await http.get(Uri.parse(url));
 
-        file.writeAsBytesSync(response.bodyBytes);
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Image downloaded successfully!'),
-          ),
-        );
+        if (response.statusCode == 200) {
+          final documentDirectory = await getApplicationDocumentsDirectory();
+          final file = File(
+              '${documentDirectory.path}/image_${DateTime.now().millisecondsSinceEpoch}.png');
+
+          file.writeAsBytesSync(response.bodyBytes);
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Image downloaded successfully!'),
+            ),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Failed to download image.'),
+            ),
+          );
+        }
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
-            content: Text('Failed to download image.'),
+            content:
+                Text('Storage permission is denied. Cannot download image.'),
           ),
         );
       }
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Storage permission is denied. Cannot download image.'),
-        ),
-      );
     }
   }
-}
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -269,42 +270,41 @@ Future<void> downloadImage(String url) async {
         // Images
         Expanded(
           child: visibleMessages.isNotEmpty &&
-              visibleMessages[0].attachments.isNotEmpty
+                  visibleMessages[0].attachments.isNotEmpty
               ? Focus(
-            focusNode: focusNode,
-            autofocus: true,
-            onKey: (FocusNode node, RawKeyEvent event) {
-              if (event is RawKeyDownEvent) {
-                if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
-                  likeCurrentImage();
-                  return KeyEventResult.handled;
-                }
-                else if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
-                  loadRandomImage();
-                  return KeyEventResult.handled;
-                }
-                else if (event.logicalKey == LogicalKeyboardKey.keyR) {
-                  loadRandomImage();
-                  return KeyEventResult.handled;
-                }
-              }
-              return KeyEventResult.ignored;
-            },
-            child: Dismissible(
-              key: Key(visibleMessages[0].id),
-              onDismissed: (direction) {
-                if (direction == DismissDirection.startToEnd) {
-                  likeCurrentImage(); // Likes the image when swiped right
-                } else if (direction == DismissDirection.endToStart) {
-                  loadRandomImage(); // Loads random image when swiped left
-                }
-              },
-              child: Center(
-                child: FetchedImageWidget(
-                    imageUrl: visibleMessages[0].attachments[0].url),
-              ),
-            ),
-          )
+                  focusNode: focusNode,
+                  autofocus: true,
+                  onKey: (FocusNode node, RawKeyEvent event) {
+                    if (event is RawKeyDownEvent) {
+                      if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+                        likeCurrentImage();
+                        return KeyEventResult.handled;
+                      } else if (event.logicalKey ==
+                          LogicalKeyboardKey.arrowLeft) {
+                        loadRandomImage();
+                        return KeyEventResult.handled;
+                      } else if (event.logicalKey == LogicalKeyboardKey.keyR) {
+                        loadRandomImage();
+                        return KeyEventResult.handled;
+                      }
+                    }
+                    return KeyEventResult.ignored;
+                  },
+                  child: Dismissible(
+                    key: Key(visibleMessages[0].id),
+                    onDismissed: (direction) {
+                      if (direction == DismissDirection.startToEnd) {
+                        likeCurrentImage(); // Likes the image when swiped right
+                      } else if (direction == DismissDirection.endToStart) {
+                        loadRandomImage(); // Loads random image when swiped left
+                      }
+                    },
+                    child: Center(
+                      child: FetchedImageWidget(
+                          imageUrl: visibleMessages[0].attachments[0].url),
+                    ),
+                  ),
+                )
               : const Center(child: CircularProgressIndicator()),
         ),
 
@@ -317,10 +317,7 @@ Future<void> downloadImage(String url) async {
               AnimatedContainer(
                 duration: const Duration(milliseconds: 200),
                 width:
-                _isSearch ? MediaQuery
-                    .of(context)
-                    .size
-                    .width * 0.4 : 50.0,
+                    _isSearch ? MediaQuery.of(context).size.width * 0.4 : 50.0,
                 height: 50.0,
                 decoration: BoxDecoration(
                   color: _isSearch ? Colors.grey[200] : Colors.white,
@@ -328,34 +325,36 @@ Future<void> downloadImage(String url) async {
                 ),
                 child: _isSearch
                     ? TextField(
-                  controller: searchController,
-                  style: const TextStyle(color: Color(0xFF2D4356)),
-                  onSubmitted: (value) {
-                    searchMessages();
-                  },
-                  decoration: InputDecoration(
-                    prefixIcon: IconButton(
-                      icon: const Icon(Icons.search, color: Color(0xFF2D4356)),
-                      onPressed: searchMessages,
-                    ),
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.close, color: Color(0xFF2D4356)),
-                      onPressed: resetSearch,
-                    ),
-                    border: const OutlineInputBorder(
-                        borderSide: BorderSide.none),
-                    contentPadding:
-                    const EdgeInsets.symmetric(horizontal: 10),
-                  ),
-                )
+                        controller: searchController,
+                        style: const TextStyle(color: Color(0xFF2D4356)),
+                        onSubmitted: (value) {
+                          searchMessages();
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: IconButton(
+                            icon: const Icon(Icons.search,
+                                color: Color(0xFF2D4356)),
+                            onPressed: searchMessages,
+                          ),
+                          suffixIcon: IconButton(
+                            icon: const Icon(Icons.close,
+                                color: Color(0xFF2D4356)),
+                            onPressed: resetSearch,
+                          ),
+                          border: const OutlineInputBorder(
+                              borderSide: BorderSide.none),
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 10),
+                        ),
+                      )
                     : IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    setState(() {
-                      _isSearch = true;
-                    });
-                  },
-                ),
+                        icon: const Icon(Icons.search),
+                        onPressed: () {
+                          setState(() {
+                            _isSearch = true;
+                          });
+                        },
+                      ),
               ),
               const SizedBox(width: 20),
               Container(
@@ -393,13 +392,21 @@ Future<void> downloadImage(String url) async {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) =>
-                                LikedImagesPage(likedMessages: likedMessages, likedImagesCounter: likedImagesCounter,)
-                        ),
+                            builder: (context) => LikedImagesPage(
+                                  likedMessages: likedMessages,
+                                  likedImagesCounter: likedImagesCounter,
+                                )),
                       );
                     });
                   },
                 ),
+              ),
+              const SizedBox(width: 10),
+              ValueListenableBuilder<int>(
+                valueListenable: likedImagesCounter,
+                builder: (context, value, child) {
+                  return Text('$value', style: const TextStyle(fontSize: 18.0));
+                },
               ),
               const SizedBox(width: 20),
               Container(
@@ -414,53 +421,12 @@ Future<void> downloadImage(String url) async {
                   borderRadius: BorderRadius.circular(100),
                 ),
                 child: IconButton(
-                  icon: const Icon(Icons.download, color: Color(0xFF2D4356)), // Download icon
+                  icon: const Icon(Icons.download, color: Color(0xFF2D4356)),
                   onPressed: () {
-                    if (visibleMessages.isNotEmpty && visibleMessages[0].attachments.isNotEmpty) {
-                      downloadImage(visibleMessages[0].attachments[0].url); // Call the download function with the current image URL
+                    if (visibleMessages.isNotEmpty &&
+                        visibleMessages[0].attachments.isNotEmpty) {
+                      downloadImage(visibleMessages[0].attachments[0].url);
                     }
-                  },
-                ),
-              ),
-
-              
-              
-              // Container(
-              //   width: 50.0,
-              //   height: 50.0,
-              //   decoration: BoxDecoration(
-              //     color: Colors.white,
-              //     border: Border.all(
-              //       color: Colors.white,
-              //       width: 2,
-              //     ),
-              //     borderRadius: BorderRadius.circular(100),
-              //   ),
-              //   child: IconButton(
-              //     icon: const Icon(Icons.whatshot, color: Color(0xFF2D4356)),
-              //     // use an appropriate icon
-              //     onPressed: () {
-              //       // Future.delayed(Duration(milliseconds: 100), () {
-              //       //   Navigator.push(
-              //       //     context,
-              //       //     MaterialPageRoute(
-              //       //         // builder: (context) => FYPPage(likedMessages: likedMessages)
-              //       //     ),
-              //       //   );
-              //       // });
-              //     },
-              //   ),
-              // ),
-
-              Padding(
-                padding: const EdgeInsets.only(left: 10.0),
-                child: ValueListenableBuilder<int>(
-                  valueListenable: likedImagesCounter,
-                  builder: (context, value, child) {
-                    return Text(
-                        '$value',
-                        style: const TextStyle(fontSize: 18.0)
-                    );
                   },
                 ),
               ),
