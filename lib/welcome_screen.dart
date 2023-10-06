79f009a29d08698fe0253c4d4d7247de2d27a542
MiletaA <avramovic.mileta@gmail.com>
2023-10-06 20:21:15 +0200

    style update

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -7,28 +7,62 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  Color bg = const Color(0xFF08576a);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 80, 72, 72),
+        backgroundColor: bg,
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 2.0),
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                ElevatedButton(
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    fixedSize: const Size(200, 50),
+                    shadowColor: Colors.black,
+                    textStyle: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 18),
+                    foregroundColor: Colors.white,
+                    side: const BorderSide(width: 2.0, color: Colors.white),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32.0),
+                    ),
+                  ),
                   child: const Text('Log In'),
                   onPressed: () {
                     Navigator.pushNamed(context, 'login_screen');
                   },
                 ),
-                ElevatedButton(
+                const SizedBox(height: 20),
+                OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      fixedSize: const Size(200, 50),
+                      textStyle: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18),
+                      foregroundColor: Colors.white,
+                      side: const BorderSide(width: 2.0, color: Colors.white),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0),
+                      ),
+                    ),
                     child: const Text('Register'),
                     onPressed: () {
                       Navigator.pushNamed(context, 'registration_screen');
                     }),
-                ElevatedButton(
+                const SizedBox(height: 20),
+                OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      fixedSize: const Size(200, 50),
+                      textStyle: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18),
+                      foregroundColor: Colors.white,
+                      side: const BorderSide(width: 2.0, color: Colors.white),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0),
+                      ),
+                    ),
                     child: const Text('Try it'),
                     onPressed: () {
                       Navigator.pushNamed(context, 'home_screen');
